c38b5290c052253504da69f7dbaec3f430ea915d
hanks <zhouhan315@gmail.com>
2015-02-12 23:46:38 +0900

    Initial Commit

---
+++ Scratch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Scratch
+//
+//  Created by hanks on 2/12/15.
+//  Copyright (c) 2015 com.hanks. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Scratch/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Scratch
+//
+//  Created by hanks on 2/12/15.
+//  Copyright (c) 2015 com.hanks. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
